93bd2cc421dab3dad7e95668d38d98c5881b7187
lbouweriks <88744104+lbouweriks@users.noreply.github.com>
2021-08-14 19:04:33 +0200

    Add files via upload

---
+++ scripts2.js
@@ -0,0 +1,28 @@
+const age=8;
+
+if (age>=18){
+    console.log("naar binnen")
+}
+else{
+    console.log("niet naar binnen")
+}
+
+const isFemale=false
+
+if (isFemale){
+    console.log("toegang tot ladies night")
+}
+else{
+    console.log("geen toegang tot ladies night")
+}
+
+const driverStatus="bob"
+
+if (driverStatus==="bob"){
+    console.log("mag rijden")
+}
+else{
+    console.log("mag niet rijden")
+}
+
+
