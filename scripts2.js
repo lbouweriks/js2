5e436e4715b45a92067fa56f73e80bb6e5eb1351
lbouweriks <88744104+lbouweriks@users.noreply.github.com>
2021-08-14 19:12:26 +0200

    Add files via upload

--- scripts2.js
+++ scripts2.js
@@ -26,3 +26,25 @@ else{
 }
 
 
+if (age>=18 && age<=25){
+    console.log("Je krijgt 50% korting!")
+}
+
+
+const firstName="Sarah"
+
+if (firstName==="Sarah" || firstName==="Bram"){
+    console.log("Je krijgt een gratis biertje")
+}
+
+const totalAmount=200
+
+if (totalAmount=>25 && totalAmount<50){
+    console.log("gratis bitterballen")
+}
+else if (totalAmount=>50 && totalAmount<100){
+    console.log("gratis nachos")
+}
+else if (totalAmount=>100){
+    console.log("gratis champagne")
+}
